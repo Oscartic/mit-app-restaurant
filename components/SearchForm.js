8c11d5487a9897a4eb056a97aca2d6c31e65f35f
Oscar Ballesteros <oscartic@gmail.com>
2022-02-06 20:19:04 -0300

    add div with id for SearchRestaurant

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -1,19 +1,23 @@
-import { Input, Row, Col } from 'antd';
+import { Input } from 'antd';
+import styles from '../styles/SearchForm.module.css'
 
 const SearchForm = () => {
 
     const onSearch = value => console.log(value);
     
     const { Search } = Input;
-    return(  
-        <Search
-            placeholder="input search text"
-            allowClear
-            enterButton="Search"
-            size="large"
-            onSearch={onSearch}
-            style={{ margin: '2rem 0'}}
-        />
+    return( 
+        <div id='search_restaurant'>
+            <Search
+                placeholder="input search text"
+                allowClear
+                enterButton="Search"
+                size="large"
+                onSearch={onSearch}
+                style={{ margin: '2rem 0'}}
+                loading={false} 
+            />
+        </div> 
     )
 };
 
