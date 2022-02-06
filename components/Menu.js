67705ec45cfcf4e5e047e52f6eb73858ad3c93ea
Oscar Ballesteros <oscartic@gmail.com>
2022-02-05 21:39:40 -0300

    build menu app

--- components/Menu.js
+++ components/Menu.js
@@ -1,9 +1,17 @@
 import { Menu, Row, Col } from 'antd';
-import { CrownOutlined, AppstoreOutlined, SettingOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import Link from 'next/Link'
+import { useRouter } from 'next/router'
+import { CrownOutlined, LoginOutlined, LogoutOutlined, SettingOutlined } from '@ant-design/icons';
 import styles from '../styles/Menu.module.css'
 import Cart from './Cart';
 
 const TopMenu = () => {
+  
+  const isActive = () => {
+    const router = useRouter();
+    return router.pathname == "/restaurants" ? styles.active : "";
+  }
+
   return (
     <>
       <div id={styles.header_title}>
@@ -11,8 +19,24 @@ const TopMenu = () => {
       </div>
       <Row style={{ margin: '2rem 0'}}>
         <Col span={16} offset={2}>
-            <CrownOutlined />
-            All Restaurants
+        <Link as={`/restaurants/`} href="restaurants/">
+            <a className={styles.main_menu} className={isActive()}>
+              <CrownOutlined />
+              Restaurants
+            </a>
+          </Link>
+          <Link as={`/restaurants/`} href="restaurants/">
+            <a className={styles.main_menu}>
+              <SettingOutlined />
+              Account
+            </a>
+          </Link>
+          <Link as={'/'} href="/">
+            <a className={styles.main_menu}>
+              <LogoutOutlined />
+              Logout
+            </a>
+          </Link>
         </Col>
         <Col span={4} >
           <Cart quantity={0} total={0} />
