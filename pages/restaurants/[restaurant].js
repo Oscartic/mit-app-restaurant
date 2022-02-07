14278558164dc3573df46c1f9e21d7bdbb45a24e
Oscar Ballesteros <oscartic@gmail.com>
2022-02-06 22:07:11 -0300

    add sub index title in restaurant dishes page

--- pages/restaurants/[restaurant].js
+++ pages/restaurants/[restaurant].js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import Dishes from '../../components/Dishes';
+import { DoubleRightOutlined } from '@ant-design/icons';
 import styles from '../../styles/Home.module.css'
 
 const Restaurant = () => {
@@ -12,7 +13,7 @@ const Restaurant = () => {
       <Head>
         <title>{ router.query.restaurant }</title>
       </Head>
-      <h1>{ router.query.restaurant }</h1>
+      <h1 className={styles.main_title}>{ router.query.restaurant } <span><DoubleRightOutlined /></span> Dishes</h1>
       <Dishes />
     </>
   )
