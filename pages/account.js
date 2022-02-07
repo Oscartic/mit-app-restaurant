e695df9d00355e4cb7cee0c2110895a94b14a677
Oscar Ballesteros <oscartic@gmail.com>
2022-02-06 22:06:33 -0300

    add text in account page

--- pages/account.js
+++ pages/account.js
@@ -1,13 +1,21 @@
 import Orders from '../components/Orders'
-import { Typography } from 'antd';
+import { Row, Col, Typography, Divider } from 'antd';
+import UserForm from '../components/UserForm';
 
 
 const Account = () => {
     const { Text } = Typography;
   return (
     <>
-        <h1> Hi, Oscar</h1>
-        <Text strong>Thanks for being a MitRestaurant customer.</Text>
+        <Row style={{margin: '1rem 0'}}>
+            <Col span={24}>
+                <h1> Hi, Oscar</h1>
+                <Text strong>Thanks for being a MitRestaurant customer.</Text>
+                <Divider>Personal info</Divider>
+                <UserForm />
+                <Divider>Order history</Divider>
+            </Col>
+        </Row>
         <Orders />
     </>
     );
