032feca12c568a22a3795688500e9c55a9aae999
oscartic <orballesteros@falabella.cl>
2022-02-08 16:47:17 -0300

    remove unused dependencies CardRestaurant component

--- pages/restaurants/index.js
+++ pages/restaurants/index.js
@@ -1,16 +1,7 @@
 import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
-import Link from 'next/Link'
 import CardsRestaurants from '../../components/CardsRestraurants'
-import SearchForm from '../../components/SearchForm'
 
 const Restaurants = () => {
-
-  const restaurants = [
-    {id: 1, name: 'WoodsHill'},
-    {id: 2, name: 'Fiorellas'},
-    {id: 3, name: 'Karma'},
-  ]
 
   return (
     <>
