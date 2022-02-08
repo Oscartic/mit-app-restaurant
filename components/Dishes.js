cf49b2cb2aa31f6624dba20117c53e01c7392043
oscartic <orballesteros@falabella.cl>
2022-02-08 16:45:27 -0300

    refactor Dishes component

--- components/Dishes.js
+++ components/Dishes.js
@@ -1,21 +1,37 @@
 import { Row } from 'antd';
+import { useEffect, useState } from 'react';
 import CardDishes from './CardDishes';
+import SkeletonCard from './skeletons/Card';
 const Dishes = () => {
-    const restaurants = [
-        {id: 1, name: 'WoodsHill', description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus egestas convallis ullamcorper."},
-        {id: 2, name: 'Fiorellas', description: "Aliquam in neque sagittis mattis ante vae lobortis lectus. Aliquam sagittis tellus ac est convallis posuere."},
-        {id: 3, name: 'Karma', description: "Fusce dignissim neque in feugiat accumsan. Praesent iaculis facilisis mattis ante vitae, lobortis semper."},
-        {id: 4, name: 'Mompelie', description: "Amet dignissim lino neque in feugiat Praesent iaculis facilisis mattis ante vitae, lobortis semper."},
+    const dishes = [
+        {id: 1, name: 'WoodsHill', description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus egestas convallis ullamcorper.", price: 5.20},
+        {id: 2, name: 'Fiorellas', description: "Aliquam in neque sagittis mattis ante vae lobortis lectus. Aliquam sagittis tellus ac est convallis posuere.", price: 1.10},
+        {id: 3, name: 'Karma', description: "Fusce dignissim neque in feugiat accumsan. Praesent iaculis facilisis mattis ante vitae, lobortis semper.", price: 3.20},
+        {id: 4, name: 'Mompelie', description: "Amet dignissim lino neque in feugiat Praesent iaculis facilisis mattis ante vitae, lobortis semper.", price: 1.00},
 
     ]
+
+    const [dishesList, setDishesList] = useState([]);
+
+    useEffect(() => {
+        if(dishesList <= 0){
+            setTimeout(()=>{
+                setDishesList(dishes);
+            },2000);
+        } 
+    }, []);
+
     return (
         <Row gutter={[16, 24]}>
         {
-            restaurants.map(item => {
+            dishesList.map(item => {
             return (
-                    <CardDishes key={item.id} name={item.name} description={item.description}/>
+                    <CardDishes key={item.id} name={item.name} description={item.description} price={item.price}/>
                 )
             })
+        }
+        {
+            dishesList <= 0 && <SkeletonCard />
         }
         </Row> 
     )
