1f327444ff97eace2dd3b292fe936a1395f58c20
Oscar Ballesteros <oscartic@gmail.com>
2022-02-11 22:10:06 -0300

    show quantity inside cart in dish card component

--- components/CardDishes.js
+++ components/CardDishes.js
@@ -1,6 +1,6 @@
 import { Card, Col } from 'antd';
 import styles from '../styles/Dishes.module.css';
-import { PlusCircleOutlined } from '@ant-design/icons';
+import { PlusCircleOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import useCart from '../Hooks/useCart';
 
 const CardRestaurant = ({dish}) => {
@@ -9,9 +9,8 @@ const CardRestaurant = ({dish}) => {
     
     const infoCart = () => {
         const item = showItemCart(dish.id);
-        if(!item) return null;
-        console.log(item)
-        return <>in cart: {item.quantity}</>
+        if(!item) return <span className={styles.dishes_cart_quantity_empty}> <ShoppingCartOutlined /> 0 </span>;
+        return <span className={styles.dishes_cart_quantity}> <ShoppingCartOutlined /> {item.quantity} </span>
     }
 
     return (
@@ -29,7 +28,7 @@ const CardRestaurant = ({dish}) => {
                 /> 
                 <span className={styles.dishes_price}>${dish.price} USD</span>
                 <span className={styles.dishes_action} onClick={() => addAmountFromRestaurant(dish)}><PlusCircleOutlined /> Add to cart </span>
-                <span className={styles.dishes_cart_quantity}>{ infoCart() }</span>
+                { infoCart ()}
             </Card>
         </Col>
     );
