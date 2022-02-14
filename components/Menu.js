7a3bbd55d35e9c3f15733b2ebbb341a7ec945c56
Oscar Ballesteros <oscartic@gmail.com>
2022-02-14 09:25:32 -0300

    [inProgress] working in user menu button

--- components/Menu.js
+++ components/Menu.js
@@ -1,13 +1,16 @@
 import { Menu, Row, Col } from 'antd';
-import Link from 'next/Link'
+import Link from 'next/Link';
 import { useRouter } from 'next/router'
 import Image from 'next/image'
 import LogoApp from '../assets/images/logo-mitRest.png'
 import { CrownOutlined, LoginOutlined, LogoutOutlined, SettingOutlined } from '@ant-design/icons';
 import styles from '../styles/Menu.module.css'
 import Cart from './Cart';
+import useFirebase from '../Hooks/useFirebase';
 
 const TopMenu = () => {
+
+  const { user, logout } = useFirebase();
   
   const isActive = (path) => {
     const router = useRouter();
@@ -20,7 +23,7 @@ const TopMenu = () => {
         <Image src={LogoApp} />
       </div>
       <Row style={{ margin: '2rem 0'}}>
-        <Col span={16} offset={2} className={styles.main_menu}>
+        <Col span={13} offset={2} className={styles.main_menu}>
         <Link as={`/restaurants/`} href="/restaurants/">
             <a className={isActive('/restaurants')} >
               <CrownOutlined />
@@ -40,7 +43,12 @@ const TopMenu = () => {
             </a>
           </Link>
         </Col>
-        <Col span={4} >
+        <Col span={5}>
+          <div className={styles.user_info}>
+            <span>hello {user.email}!</span>
+          </div>
+        </Col>
+        <Col span={2} >
           <Cart quantity={0} total={0} />
         </Col>
       </Row>
