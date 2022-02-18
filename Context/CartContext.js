564b02c9b57fa2c6deff99902bc8da62f6b799a8
Oscar Ballesteros <oscartic@gmail.com>
2022-02-17 23:52:01 -0300

    remove dummy data

--- Context/CartContext.js
+++ Context/CartContext.js
@@ -4,12 +4,6 @@ export const CartContext = createContext();
 
 const CartProvider = (props) => {
 
-    const data = [
-        {id: 1, quantity: 1, price: 5.13, dishName: 'Swsopwaal', restaurantId: 2, restaurantName: 'WoodsHill', description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus egestas convallis ullamcorper."},
-        {id: 2, quantity: 2, price: 2.99, dishName: 'Logua Logua', restaurantId: 2, restaurantName: 'WoodsHill', description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus egestas convallis ullamcorper."},
-        {id: 4, quantity: 4, price: 1.11, dishName: 'Mulan', restaurantId: 2, restaurantName: 'Karma', description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus egestas convallis ullamcorper."}
-    ];
-
     const [itemsCart, setItemsCart] = useState([]);
 
     useEffect(() => {
