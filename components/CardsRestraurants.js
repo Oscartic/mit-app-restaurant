885d88457fa81502fc4f8769b7148e088044c5b2
Oscar Ballesteros <oscartic@gmail.com>
2022-02-17 21:54:19 -0300

    add axios, remove dummy data and add function server call

--- components/CardsRestraurants.js
+++ components/CardsRestraurants.js
@@ -3,23 +3,31 @@ import { useEffect, useState } from 'react';
 import CardRestaurant from './CardRestaurant';
 import SearchForm from './SearchForm';
 import SkeletonCard from './skeletons/Card';
+import axios from 'axios';
+
 const CardsRestaurants = () => {
-    const dataRestaurants = [
-        {id: 1, name: 'WoodsHill', description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus egestas convallis ullamcorper."},
-        {id: 2, name: 'Fiorellas', description: "Aliquam in neque sagittis mattis ante vae lobortis lectus. Aliquam sagittis tellus ac est convallis posuere."},
-        {id: 3, name: 'Karma', description: "Fusce dignissim neque in feugiat accumsan. Praesent iaculis facilisis mattis ante vitae, lobortis semper."},
-        {id: 4, name: 'Mompelie', description: "Amet dignissim lino neque in feugiat Praesent iaculis facilisis mattis ante vitae, lobortis semper."},
-    ];
+
     const [restaurants, setRestaurants] = useState([]);
     const [renderList, setRenderList] = useState([]);
     const [isFetch, setIsFetch] = useState(true);
     const [error, setError] = useState('');
 
+    const fetchRestaurants = async () => {
+        try {
+            const{ status, data } = await axios.get('http://localhost:5001/api/restaurants'); 
+            if(status !== 200) return console.log("error in api");
+            setRestaurants(data.restaurants);
+            setRenderList(data.restaurants) 
+        } catch (error) {
+            console.log("[CardsRestaurants][fetchRestaurants] error >>> ", error);
+        }
+    };
+
     useEffect(() => {
-        if(restaurants.length <= 0) { 
+        if(restaurants.length <= 0 || renderList.length <= 0) { 
+            
             setTimeout(() => {
-                setRestaurants(dataRestaurants);
-                setRenderList(dataRestaurants);
+                fetchRestaurants();
                 setIsFetch(false);
             }, 2000);
         }
