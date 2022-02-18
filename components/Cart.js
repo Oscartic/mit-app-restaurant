2c873c4377997da9710e206fdfb39f085a59ff9f
Oscar Ballesteros <oscartic@gmail.com>
2022-02-18 20:44:29 -0300

    add btn pay

--- components/Cart.js
+++ components/Cart.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Drawer, Empty } from "antd";
-import { ShoppingCartOutlined } from '@ant-design/icons';
+import { Drawer, Empty, Button } from "antd";
+import { ShoppingCartOutlined, CreditCardOutlined } from '@ant-design/icons';
 import styles from '../styles/Cart.module.css'
 import ItemsCart from "./ItemsCart";
 import useCart from "../Hooks/useCart";
@@ -65,10 +65,14 @@ const Cart = () => {
                     <span className={styles.text}>Estimated total: </span>
                     <span className={styles.value}>${totalCart()}</span>
                 </div>
+                
                 {
                     itemsCart.length <= 0 &&
                     <Empty description="You still do not have selected dishes"/>
                 }
+                <Button type="primary" block onClick={() => alert("Vamos a pagar")}>
+                    <CreditCardOutlined /> Checkout all Dishes
+                </Button>
             </Drawer>
         </div>
     );
