2029fcaf1538149ee5c2a2fb16b125492a9e5ee0
Oscar Ballesteros <oscartic@gmail.com>
2022-02-20 20:35:06 -0300

    add context info, components and logic for purchase flow

--- components/Cart.js
+++ components/Cart.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { Drawer, Empty, Button } from "antd";
+import { Drawer, Empty, Button, Result } from "antd";
 import { ShoppingCartOutlined, CreditCardOutlined } from '@ant-design/icons';
 import styles from '../styles/Cart.module.css'
 import ItemsCart from "./ItemsCart";
 import useCart from "../Hooks/useCart";
 import StripeCheckout from 'react-stripe-checkout';
 import axios from 'axios'; 
+import PaymentModal from "./PaymentModal";
 
 const Cart = () => {
 
-    const { itemsCart, setItemsCart, totalCart, totalItems } = useCart();
+    const { itemsCart, setItemsCart, totalCart, totalItems, orderSummary, setOrderSummary, setShowModalOrder} = useCart();
 
     const [visible, setVisible] = useState(false);
+    const [orderFetch, setOrderFetch] = useState(false);
+    const [wasSuccessful, setWasSuccessful] = useState(false);
 
     const showDrawer = () => {
         setVisible(true);
@@ -24,12 +27,18 @@ const Cart = () => {
     const makePayment = async token => {
 
         try {
+            setOrderFetch(true);
+            setShowModalOrder(true);
             const respond = await axios.post(`${process.env.API_MIT_RESTAURANT_URL}/orders/payment`, {
-                    token,
-                    itemsCart
-                }
-            );
-            console.log(respond);    
+                token,
+                itemsCart
+            });
+
+            if(respond.status === 200) {
+                setWasSuccessful(true);
+                setOrderSummary(respond.data.order);
+            }
+            setOrderFetch(false);
         } catch (error) {
             console.log(error);
         }
@@ -86,17 +95,28 @@ const Cart = () => {
                     itemsCart.length <= 0 &&
                     <Empty description="You still do not have selected dishes"/>
                 }
-                <StripeCheckout
-                    name="MitRestaurants"
-                    stripeKey={process.env.STRIPE_KEY}
-                    token={makePayment}
-                    amount={totalCart() * 100}
-                >
-                    <Button type="primary" block>
-                        <CreditCardOutlined /> Checkout all Dishes
-                    </Button>
-                </StripeCheckout>
+                { itemsCart.length > 0 &&
+                    <>
+                        <StripeCheckout
+                            name="MitRestaurants"
+                            stripeKey={process.env.STRIPE_KEY}
+                            token={makePayment}
+                            amount={totalCart() * 100}
+                        >
+                            <Button type="primary" block>
+                                <CreditCardOutlined /> Checkout all Dishes
+                            </Button>
+                        </StripeCheckout>
 
+                        <PaymentModal 
+                            orderFetch={orderFetch} 
+                            setOrderFetch={setOrderFetch} 
+                            wasSuccessful={wasSuccessful}
+                            orderSummary={orderSummary}
+                            setVisible={setVisible}
+                        />    
+                    </>
+                }
             </Drawer>
         </div>
     );
