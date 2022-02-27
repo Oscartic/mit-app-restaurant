fac4f4f7a0dc1a8fb94f13e444bfacc6b0a44085
Oscar Ballesteros <oscartic@gmail.com>
2022-02-27 01:25:34 -0300

    set data user and show info in component

--- pages/account.js
+++ pages/account.js
@@ -1,19 +1,12 @@
-import Orders from '../components/Orders'
-import { Row, Col, Typography, Divider } from 'antd';
-import UserForm from '../components/UserForm';
+import { Row, Col } from 'antd';
+import AccountInfo from '../components/AccountInfo';
 
 
 const Account = () => {
-    const { Text } = Typography;
   return (
     <Row style={{margin: '1rem 0'}}>
         <Col span={24}>
-            <h1> Hi, Oscar</h1>
-            <Text strong>Thanks for being a MitRestaurant customer.</Text>
-            <Divider>Personal info</Divider>
-            <UserForm />
-            <Divider>Order history</Divider>
-            <Orders />
+            <AccountInfo />
         </Col>
     </Row>
     );
